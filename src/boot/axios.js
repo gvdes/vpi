c9f806fa46dc0ba6e4e5f81069539f08f7ccaaf4
Eduardo Lopez <lalo.kob@gmail.com>
2023-04-10 14:07:05 -0600

    Se agrega la nueva ip del servidor

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -8,7 +8,7 @@ import axios from 'axios'
 // "export default () => {}" function below (which runs individually
 // for each client)
 // const vapi = axios.create({ baseURL: 'http://192.168.12.183/vmedia/public/api/verificator' })
-const vapi = axios.create({ baseURL: 'http://192.168.10.15:8100/vmedia/public/api/verificator' });
+const vapi = axios.create({ baseURL: 'http://192.168.10.189:8100/vmedia/public/api/verificator' });
 
 export default boot(({ app }) => {
   // for use inside Vue files (Options API) through this.$axios and this.$api
